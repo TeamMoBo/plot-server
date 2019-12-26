90cfc7a4b11b2d85c7df30f44c87823c8d92c8e1
이다현 <xnpsldnjs21@gmail.com>
2019-12-27 05:59:29 +0900

    ticket 시도

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,5 +3,6 @@ var router = express.Router();
 
 router.use("/user", require("./user"));
 router.use("/mypage", require("./mypage"));
+//router.use("/ticket", require("./ticket"));
 
 module.exports = router;
