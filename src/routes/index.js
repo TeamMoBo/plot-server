8707ac27764ed7e9e2c96ef54d3e90a717a27fe8
hhiyeon <hhyyeon0214@gmail.com>
2019-12-29 03:21:33 +0900

    Update userDao

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,5 +4,7 @@ var router = express.Router();
 router.use("/user", require("./user"));
 router.use("/mypage", require("./mypage"));
 router.use("/matching", require("./matching"))
+router.use('/main', require('./main'));
+router.use('/movie', require('./movie'));
 
 module.exports = router;
