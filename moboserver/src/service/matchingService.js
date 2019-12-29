7271aeb1f35e4ab33f3ae35a0a86d88d1e9f3114
Sangyun <winnery93@gmail.com>
2019-12-29 10:32:20 +0900

    revert 삭제된 파일

--- moboserver/src/service/matchingService.js
+++ moboserver/src/service/matchingService.js
@@ -5,7 +5,7 @@ const { verify } = require('../library/jwt');
 const matchingDao = require("../dao/matchingDao");
 const movieReservationDao = require("../dao/movieReservationDao");
 const userDao = require("../dao/userDao");
-const hashTagDao = require("../dao/hashtagDao");
+ 
 
 /**
  * 매칭 페이지
@@ -26,7 +26,8 @@ function findOpponentIdx(matchingArr, userIdx) {
 }
 
 async function getMatching(token) {
-    const userIdx = verify(token).idx;
+    // const userIdx = verify(token).idx;
+    const userIdx = 30;
     const userData = await userDao.selectUserByIdx(userIdx);
     if(userData.length == 0) {
         return -1;
@@ -38,52 +39,25 @@ async function getMatching(token) {
     const opponentUserIdx = findOpponentIdx(userMatchingData, userIdx);
     const opponentUserData = await userDao.selectUserByIdx(opponentUserIdx);
     const meetingStatus = await movieReservationDao.selectMovieReservationByMatchingidx(userMatchingData[0].matchingIdx);
-    
-    const genreTag = await hashTagDao.selectGenreTagByUseridx(userIdx);
-    const genreTagContent = genreTag.map((data) => {
-        return data['preferGenreTagName'];
-    })
-    const charmingTag = await hashTagDao.selectCharmingTagByUseridx(userIdx);
-    const charmingTagContent = charmingTag.map((data) => {
-        return data['attractPointTagName'];
-    })
-    const favorTag = await hashTagDao.selectFavorTagByUserIdx(userIdx);
-    const favorTagContent = favorTag.map((data) => {
-        return data.favorTagName;
-    })
     const movieInfoText = String(moment(meetingStatus[0].movieDate).format('YYYY-MM-DD')) + " " + meetingStatus[0].movieTime + " \n " + 
     meetingStatus[0].moviePlace + " \n " + meetingStatus[0].movieSeat;
     
     let parsingUser = {
         name : opponentUserData[0].userName,
-        img : opponentUserData[0].userImg,
         age : opponentUserData[0].userAge,
         school : opponentUserData[0].userSchool,
         major : opponentUserData[0].userMajor,
         location : opponentUserData[0].userLocation,
-        genreHash : genreTagContent,
-        charmingHash : charmingTagContent,
-        favorHash : favorTagContent,
-        moiveInfo : movieInfoText   
+        // genreHash : 
+        moiveInfo : movieInfoText,
+        img : opponentUserData[0].userImg,
+        ticket : opponentUserData[0].userTicket,
+        popcorn : opponentUserData[0].userPopcorn
     }
 
     return parsingUser;
 }
 
-async function postMatchingConfirm(token, reply) {
-    const userIdx = verify(token).idx;
-    const userData = await userDao.selectUserByIdx(userIdx);
-    if(userData.length == 0) {
-        return -1;
-    }
-    
-    const userMatchingData = await matchingDao.selectMatchingByUseridx(userIdx); //시간대 + 상태 확인 안
-    // if(userMatchingData[0])
-    
-    
-}
-
 module.exports = {
-    getMatching,
-    postMatchingConfirm
+    getMatching
 }
