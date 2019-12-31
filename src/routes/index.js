0f3ecdd5825e705dc467151fb43b11ecd5d4f582
jaeyong <maxx1027@naver.com>
2019-12-31 10:27:37 +0900

    index router 주석풀기

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var router = express.Router();
 
-//router.use("/user", require("./user"));
+router.use("/user", require("./user"));
 router.use("/mypage", require("./mypage"));
 router.use("/matching", require("./matching"));
 
