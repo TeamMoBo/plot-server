e8e03b768c269a6c5c93b2cf14f6cf30c378f397
hhiyeon <hhyyeon0214@gmail.com>
2020-01-01 17:48:53 +0900

    메인페이지 조회 수정

--- src/service/movieService.js
+++ src/service/movieService.js
@@ -1,4 +1,5 @@
 const movieDao = require('../dao/movieDao');
+const moment = require('moment');
 
 /*
 1. 영화 널값인지 확인
@@ -6,13 +7,101 @@ const movieDao = require('../dao/movieDao');
 3. 이미지, 이름, 별점
 */
 
-async function getMovie(movieReleaseStatus) {
-    const movieData = await movieDao.selectMovie(movieReleaseStatus);
-    
-    if(!movieData){  // 입력 값이 null일 경우
-        return -1;
+async function getOrderMovie(movieReleaseStatus) {
+    const movieData = {
+        "movieReleaseStatus" : 0,
+        "movieData" : []
+    };
+
+    if(movieReleaseStatus == 0){    // 현재 상영작
+        const currentMovieData = await movieDao.selectCurrentMovie();
+        movieData.movieReleaseStatus = currentMovieData[0].movieReleaseStatus;
+
+        for(let i = 0; i<currentMovieData.length; i++){
+            let movieArr = {
+                "movieIdx" : 0,
+                "movieName" : "",
+                "movieScore" : 0,
+                "movieImg" : ""
+            }
+            movieArr.movieIdx = currentMovieData[i].movieIdx;
+            movieArr.movieName = currentMovieData[i].movieName;
+            movieArr.movieScore = currentMovieData[i].movieScore;
+            movieArr.movieImg = currentMovieData[i].movieImg;
+            movieData.movieData.push(movieArr);
+        }
     }
-    
+
+    if(movieReleaseStatus == 1){    // 개봉 예정작
+        const futureMovieData = await movieDao.selectFutureMovie();
+        movieData.movieReleaseStatus = futureMovieData[0].movieReleaseStatus;
+
+        for(let i = 0; i<futureMovieData.length; i++){
+            let movieArr = {
+                "movieIdx" : 0,
+                "movieName" : "",
+                "movieScore" : 0,
+                "movieImg" : ""
+            }
+            movieArr.movieIdx = futureMovieData[i].movieIdx;
+            movieArr.movieName = futureMovieData[i].movieName;
+            movieArr.movieScore = futureMovieData[i].movieScore;
+            movieArr.movieImg = futureMovieData[i].movieImg;
+            movieData.movieData.push(movieArr);
+        }
+    }
+    return movieData;
+}
+
+
+async function getReserveMovie(userIdx) {
+    const selectMovieResult = await movieDao.selectMovieReserveMovie(userIdx);
+    const selectTimeResult = await movieDao.selectMovieReserveDate(userIdx);
+
+    const movieData = {
+        "reservationIdx" : 0,
+        "userIdx" : 0,
+        "movieIdx" : [],
+        "reservationDate" : "",
+        "reservationTime" : []
+    };
+        
+    movieData.reservationIdx = selectMovieResult[0].reservationIdx;
+    movieData.userIdx = userIdx;
+    movieData.reservationDate = selectTimeResult[0].reservationDate;
+
+    // movie filtering
+    let movieLength = Object.keys(selectMovieResult);
+    let movieArr = [];
+
+    for(let i = 0; i<movieLength.length; i++){
+        movieArr.push(selectMovieResult[i].movieIdx);
+    }
+
+    let movieFilterArr = movieArr.filter((item, idx, array) =>{
+        return array.indexOf(item) === idx;
+    });
+
+    for(let i = 0; i<movieFilterArr.length; i++){
+        movieData.movieIdx.push(movieFilterArr[i])
+    }
+
+
+    // time filtering
+    let timeLength = Object.keys(selectTimeResult);
+    let timeArr = [];
+
+    for(let j = 0; j<timeLength.length; j++){
+        timeArr.push(selectTimeResult[j].reservationTime);
+    }
+
+    let timeFilterArr = timeArr.filter((item, idx, array) =>{
+        return array.indexOf(item) === idx;
+    });
+
+    for (let j = 0; j<timeFilterArr.length; j++){
+        movieData.reservationTime.push(timeFilterArr[j])
+    }
     return movieData;
 }
 
@@ -21,27 +110,82 @@ async function getMovie(movieReleaseStatus) {
 2. 최소 몇개 이상 선택해야 하는 것
 */
 async function postMovie(userIdx, movieData) {
-    const infoData = await movieDao.insertMovie(userIdx, movieData);
-
-    console.log(infoData)
-    if(!infoData){    // 입력 값이 null일 경우
+    if(movieData.reservationTime.length < 3){   // reservationTime을 3개 이상 받지 않았을 경우 = -4
         return -1;
     }
-    return infoData;
+
+    const weekdayArr = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];  /// weekday data set
+    const weekday = weekdayArr[new Date(movieData.reservationDate).getDay()];
+    
+    const reservationResult = await movieDao.insertReservation(userIdx, movieData, weekday);
+    const reservationIdx = reservationResult.insertId;
+
+    if(!reservationResult){ // 쿼리가 제대로 수행되지 않았을 경우
+        return -4;
+    }
+
+    const idxLength = Object.keys(movieData.movieIdx);
+    const timeLength = Object.keys(movieData.reservationTime);
+    
+    for(let i = 0; i<idxLength.length; i++){
+        const movieResult = await movieDao.insertReservationMovie(reservationIdx, movieData.movieIdx[i]);
+
+        if(!movieResult){   // 쿼리가 제대로 수행되지 않았을 경우
+            return -4;
+        }
+    }
+
+    for(let i = 0; i<timeLength.length; i++){
+        const timeResult = await movieDao.insertReservationTime(reservationIdx, movieData.reservationTime[i]);
+
+        if(!timeResult){    // 쿼리가 제대로 수행되지 않았을 경우
+            return -4;
+        }
+    }
+    return 0;
 }
 
-async function putMovie(userIdx, movieData) {
-    const infoData = await movieDao.updateMovie(userIdx, movieData);
 
-    console.log(infoData)
-    if(!infoData){    // 입력 값이 null일 경우
+async function putMovie(userIdx, movieData) {
+    if(movieData.reservationTime.length < 3){   // reservationTime을 3개 이상 받지 않았을 경우 = -4
         return -1;
     }
-    return infoData;
+
+    const weekdayArr = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];  /// weekday data set
+    const weekday = weekdayArr[new Date(movieData.reservationDate).getDay()];
+    
+    const reservationResult = await movieDao.updateReservation(movieData, weekday, userIdx);
+
+    if(!reservationResult){ // 쿼리가 제대로 수행되지 않았을 경우
+        return -4;
+    }
+
+    // body가 더 많을경우 INSERT도 해야함,, 없으면 DELETE? UPDATE = 0?
+    
+    const idxLength = Object.keys(movieData.movieIdx);
+    const timeLength = Object.keys(movieData.reservationTime);
+    
+    for(let i = 0; i<idxLength.length; i++){
+        const movieResult = await movieDao.insertReservationMovie(reservationIdx, movieData.movieIdx[i]);
+
+        if(!movieResult){   // 쿼리가 제대로 수행되지 않았을 경우
+            return -4;
+        }
+    }
+
+    for(let i = 0; i<timeLength.length; i++){
+        const timeResult = await movieDao.insertReservationTime(reservationIdx, movieData.reservationTime[i]);
+
+        if(!timeResult){    // 쿼리가 제대로 수행되지 않았을 경우
+            return -4;
+        }
+    }
+    return 0;
 }
 
 module.exports = {
-    getMovie,
+    getOrderMovie,
+    getReserveMovie,
     postMovie,
     putMovie
 }
