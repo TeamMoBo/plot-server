372d0038eed2a762760d5a8f6ebf177f930d7c12
hhiyeon <hhyyeon0214@gmail.com>
2020-01-01 17:49:25 +0900

    예매순 영화조회, 예약 조회 추가

--- src/routes/movie.js
+++ src/routes/movie.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 var movieController = require('../controller/movieController.js')
 
-router.get('/:movieReleaseStatus', movieController.getMovie);    // 예매율 TOP10 영화조회(현재상영작,개봉예정작)
+router.get('/:movieReleaseStatus', movieController.getOrderMovie);    // 예매율 TOP10 영화조회(현재상영작,개봉예정작)
+router.get('/', movieController.getReserveMovie);    // 영화 조회
 router.post('/', movieController.postMovie);    // 영화 선택
 router.put('/', movieController.putMovie); // 영화 수정
 
